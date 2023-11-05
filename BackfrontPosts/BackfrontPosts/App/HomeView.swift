6f80a76e484765db37c93dd2c5df3efc67a5d2ff
Caio Fabrini <caiofabrini@outlook.com>
2023-11-05 14:16:11 -0300

    ajustes HomeView

--- BackfrontPosts/BackfrontPosts/App/HomeView.swift
+++ BackfrontPosts/BackfrontPosts/App/HomeView.swift
@@ -2,80 +2,89 @@
 //  HomeView.swift
 //  InstagramClone
 //
-//  Created by Dheeraj Kumar Sharma on 02/12/20.
+//  Caio
 //
 
 import SwiftUI
 
 struct HomeView: View {
-    // MARK:- PROPERTIES
-    
-    init(){
-        UINavigationBar.appearance().barTintColor = .white
-        UINavigationBar.appearance().shadowImage = UIImage()
-    }
-    
-    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
     
     // MARK:- BODY
     
     var body: some View {
-        NavigationView {
-            ScrollView(.vertical, showsIndicators:false) {
-                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 0){
-                    StoryView(storyData: StoryData)
-                    Divider()
-                    ForEach(PostData) { item in
-                        PostView(postData: item)
+        NavigationStack {
+            ZStack {
+                Color(red: 237/255, green: 237/255, blue: 237/255)
+                HStack(spacing: 20.0) {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVStack {
+                            ForEach(StoryData) { item in
+                              StoryView(storyData: item)
+                            }
+                        }
                     }
-                }//: GRID
-            }//: SCROLL
-            .navigationBarTitle("", displayMode: .inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Image("logo")
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height:65)
-                    }//: TOOLBAR ITEM LEFT
-        
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        HStack(alignment:.center, spacing:25){
-                            Button(action:{
-                                print("Go to Search")
-                            }){
-                                Image("search")
+                    .background(Color.white)
+                    .frame(width: 90)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVStack(spacing: 20) {
+                            ForEach(PostData) { item in
+                              PostView(postData: item)
+                            }
+                        }
+                    }
+
+                }
+                .padding([.top, .leading, .trailing])
+                .padding(.bottom, 5)
+            }
+            .toolbarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("logo")
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height:65)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(alignment:.center, spacing: 25) {
+                        Button(action:{
+                            print("Go to Search")
+                        }){
+                            Image("search")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 25, height: 25)
+                        }
+                        
+                        Button(action: {
+                            print("Go to messages")
+                        }){
+                            ZStack{
+                                Image("message")
                                     .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 25, height: 25, alignment: .center)
+                                    .scaledToFit()
+                                    .frame(width: 25, height: 25)
+                                
+                                Text("3")
+                                    .font(Font.system(size: 13, weight: .bold))
+                                    .frame(width: 18, height: 18)
+                                    .background(Color.red)
+                                    .foregroundStyle(.white)
+                                    .clipShape(Circle())
+                                    .offset(
+                                        x: 9,
+                                        y: -9
+                                    )
                             }
-                            
-                            Button(action:{
-                                print("Go to messages")
-                            }){
-                                ZStack{
-                                    Image("message")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 25, height: 25, alignment: .center)
-                                    
-                                    Text("3")
-                                        .font(Font.system(size: 13, weight: .bold))
-                                        .frame(width:18, height:18)
-                                        .background(Color.red)
-                                        .foregroundColor(.white)
-                                        .clipShape(Circle())
-                                        .offset(
-                                            x: 9,
-                                            y: -9
-                                        )
-                                }//: ZSTACK
-                            }
-                        }//: HSTACK
-                    }//: TOOLBAR ITEM RIGHT
-                }//: TOOLBAR
-        }//: NAVIGATION VIEW
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- BackfrontPosts/BackfrontPosts/View/PostView.swift
+++ BackfrontPosts/BackfrontPosts/View/PostView.swift
@@ -24,229 +24,158 @@ struct PostView: View {
         }
     }
     
-    func estimatedHeight(_ width:Float , _ height: Float) -> CGFloat {
-        let ratio = CGFloat(height) / CGFloat(width)
-        let estimatedH = ratio * UIScreen.main.bounds.width
-        return estimatedH
-    }
-    
     // MARK:- BODY
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            HStack(alignment: .center, spacing: 10){
-                Image(postData.profileImage)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 33, height: 33, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(
-                        Circle().stroke(Color.gray, lineWidth: 0.5)
-                    )
-                
-                if postData.location != nil && postData.isSponsored == false {
-                    VStack(alignment: .leading, spacing: 1){
-                        Text(postData.userName)
-                            .font(Font.system(size: 14, weight: .semibold))
-                        Text(postData.location)
-                            .font(Font.system(size: 12))
-                    }
-                } else if postData.isSponsored {
-                    VStack(alignment: .leading, spacing: 1){
-                        Text(postData.userName)
-                            .font(Font.system(size: 14, weight: .semibold))
-                        Text("Sponsored")
-                            .font(Font.system(size: 12))
-                    }
-                } else {
+        VStack {
+                HStack(alignment: .center, spacing: 10) {
+                    Image(postData.profileImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 33, height: 33, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.gray, lineWidth: 0.5)
+                        )
                     Text(postData.userName)
                         .font(Font.system(size: 14, weight: .semibold))
-                }
+                    
+                    Spacer()
+                    Button(action:{
+                        print("options")
+                    }){
+                        Image("more")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 30, height: 30, alignment: .center)
+                    }
+                }//: HSTACK
+                .padding(.vertical, 10)
+                .padding(.horizontal, 12)
                 
-                Spacer()
-                Button(action:{
-                    print("options")
-                }){
-                    Image("more")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30, alignment: .center)
-                }
-            }//: HSTACK
-            .padding(.vertical, 10)
-            .padding(.horizontal, 12)
-            
-            Divider()
-            
-            if !postData.isVideo {
-                ZStack(alignment: .center){
-                    Image(postData.postImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: UIScreen.main.bounds.width, height: estimatedHeight(postData.dimensionImage.width,postData.dimensionImage.height))
-                        .clipped()
-                        .onTapGesture(count: 2) {
-                            postData.isLiked = true
-                            isLikeAnimation = true
-                            hideAnimation()
-                        }
-                    
-                    Image("white-heart")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 170, height: 170)
-                        .scaleEffect(isLikeAnimation ? 1 : 0)
-                        .opacity(isLikeAnimation ? 1 : 0)
-                        .animation(.spring())
-                    
-                }//: ZSTACK
-            } else {
-                ZStack(alignment: .center){
-                    
-                    if Bundle.main.url(forResource: postData.videoUrl, withExtension: "mp4") != nil {
-                        PlayerView(url: Bundle.main.url(forResource: postData.videoUrl, withExtension: "mp4")!, isMute: $isMute)
-                           .frame(width: UIScreen.main.bounds.width, height: estimatedHeight(postData.dimensionVideo.width,postData.dimensionVideo.height))
+                if !postData.isVideo {
+                    ZStack(alignment: .center) {
+                        Image(postData.postImage)
+                            .resizable()
+                            .scaledToFit()
                             .onTapGesture(count: 2) {
                                 postData.isLiked = true
                                 isLikeAnimation = true
                                 hideAnimation()
                             }
+                        
+                        Image("white-heart")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 170, height: 170)
+                            .scaleEffect(isLikeAnimation ? 1 : 0)
+                            .opacity(isLikeAnimation ? 1 : 0)
+                            .animation(.spring())
+                        
                     }
-                    
-                    Image("white-heart")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 170, height: 170)
-                        .scaleEffect(isLikeAnimation ? 1 : 0)
-                        .opacity(isLikeAnimation ? 1 : 0)
-                        .animation(.spring())
-                }//: ZSTACK
-                .background(Color(red:230/255, green:230/255 ,blue:230/255))
-                .overlay(
-                    Button(action:{
-                        isMute.toggle()
-                    }) {
-                        ZStack{
-                            Circle()
-                                .fill(Color(red:0, green:0, blue:0))
-                                .frame(width:30, height:30)
-                                .opacity(0.8)
-                            
-                            Image(isMute ? "mute" : "sound")
-                                .resizable()
-                                .renderingMode(.template)
-                                .foregroundColor(.white)
-                                .scaledToFill()
-                                .frame(width: 17, height: 17)
-                                .clipShape(Circle())
+                } else {
+                    ZStack(alignment: .center) {
+                        if Bundle.main.url(forResource: postData.videoUrl, withExtension: "mp4") != nil {
+                            PlayerView(url: Bundle.main.url(forResource: postData.videoUrl, withExtension: "mp4")!, isMute: $isMute)
+                                .frame(minHeight: 350)
+                                .onTapGesture(count: 2) {
+                                    postData.isLiked = true
+                                    isLikeAnimation = true
+                                    hideAnimation()
+                                }
                         }
+                        
+                        Image("white-heart")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 170, height: 170)
+                            .scaleEffect(isLikeAnimation ? 1 : 0)
+                            .opacity(isLikeAnimation ? 1 : 0)
+                            .animation(.spring())
+                    }//: ZSTACK
+                    .overlay(
+                        Button(action:{
+                            isMute.toggle()
+                        }) {
+                            ZStack{
+                                Circle()
+                                    .fill(Color(red:0, green:0, blue:0))
+                                    .frame(width:30, height:30)
+                                    .opacity(0.8)
+                                
+                                Image(isMute ? "mute" : "sound")
+                                    .resizable()
+                                    .renderingMode(.template)
+                                    .foregroundColor(.white)
+                                    .scaledToFill()
+                                    .frame(width: 17, height: 17)
+                                    .clipShape(Circle())
+                            }
+                        }
+                            .padding(13)
+                        ,alignment: .bottomTrailing
+                    )
+                }
+                HStack(alignment: .center, spacing: 10){
+                    Button(action:{
+                        postData.isLiked = !postData.isLiked
+                    }){
+                        Image(postData.isLiked ? "like-selected" : "like")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 30, height: 30, alignment: .center)
                     }
-                    .padding(13)
-                    ,alignment: .bottomTrailing
-                )
-            }
-            
-            if postData.isSponsored {
-                Button(action:{
                     
-                }){
-                    HStack(alignment: .center){
-                        Text("Learn more")
-                            .font(Font.system(size:14, weight: .medium))
-                            .foregroundColor(.blue)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.blue)
-                            .font(Font.system(size: 14, weight: .medium))
+                    Button(action:{
+                        
+                    }){
+                        Image("comment")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 30, height: 30, alignment: .center)
                     }
-                    .padding(.vertical, 13)
-                    .padding(.horizontal, 15)
-                }
-                Divider()
-                    .padding(.horizontal , 15)
-            }
-            
-            HStack(alignment: .center, spacing: 10){
-                Button(action:{
-                    postData.isLiked = !postData.isLiked
-                }){
-                    Image(postData.isLiked ? "like-selected" : "like")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30, alignment: .center)
-                }
-                
-                Button(action:{
                     
-                }){
-                    Image("comment")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30, alignment: .center)
-                }
-                
-                Button(action:{
+                    Button(action:{
+                        
+                    }){
+                        Image("share")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 30, height: 30, alignment: .center)
+                    }
                     
-                }){
-                    Image("share")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30, alignment: .center)
-                }
-                
-                Spacer()
-                
-                Button(action:{
-                    postData.isSaved = !postData.isSaved
-                }){
-                    Image(postData.isSaved ? "save-selected" : "save")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30, alignment: .center)
-                }
-            }//: HSTACK
-            .padding(.vertical, 8)
-            .padding(.horizontal, 15)
-            
-            if postData.likes != 0 && !postData.isVideo {
-                Text("\(postData.likes) likes")
-                    .font(Font.system(size: 14, weight: .semibold))
-                    .padding(.horizontal, 15)
-            } else if postData.views != 0 {
-                Text("\(postData.views) views")
-                    .font(Font.system(size: 14, weight: .semibold))
-                    .padding(.horizontal, 15)
-            }
-            
-            if postData.caption != nil {
-                Group {
-                    Text(postData.userName)
-                        .font(Font.system(size: 14, weight: .semibold))
-                    + Text(" ")
-                    + Text(postData.caption)
-                        .font(Font.system(size: 14))
-                }
+                    Spacer()
+                    
+                    Button(action:{
+                        postData.isSaved = !postData.isSaved
+                    }){
+                        Image(postData.isSaved ? "save-selected" : "save")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 30, height: 30, alignment: .center)
+                    }
+                }//: HSTACK
+                .padding(.vertical, 8)
                 .padding(.horizontal, 15)
-                .padding(.vertical, 6)
-            }
-            
-            if !postData.isSponsored {
-                Text(postData.time)
-                    .foregroundColor(.gray)
-                    .font(Font.system(size: 13, weight: .medium))
+                if postData.caption != nil {
+                    Group {
+                        Text(postData.userName)
+                            .font(Font.system(size: 14, weight: .semibold))
+                        + Text(" ")
+                        + Text(postData.caption)
+                            .font(Font.system(size: 14))
+                    }
                     .padding(.horizontal, 15)
-                    .padding(.vertical, 7)
+                    .padding(.vertical, 6)
+                }
             }
-            
-        }//: VSTACK
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
     }
-}
 
 // MARK:- PREVIEW
 
-struct PostView_Previews: PreviewProvider {
-    static var previews: some View {
-        PostView(postData: PostData[4])
-            .previewLayout(.sizeThatFits)
-    }
+#Preview {
+    PostView(postData: PostData[0])
 }
